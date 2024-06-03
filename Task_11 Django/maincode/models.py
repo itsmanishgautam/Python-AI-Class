eedb84ec819e355c89e88cb042a1ace7dffaace5
Manish Gautam <itsmanishgautam@gmail.com>
2024-06-03 10:16:32 +0545

    changes

--- Task_11 Django/maincode/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Transaction(models.Model):
-    date = models.DateField()
-    bank = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f'{self.date} - {self.bank} - {self.amount}'
--- Task_11 Django/maincode/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.transaction_create, name='transaction_create'),
-    path('success/', views.transaction_success, name='transaction_success'),
-]
